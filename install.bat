919d6864846900fe7fff0045fffc401b4f405b1c
Taladro678 <luvinrafael@gmail.com>
2025-12-19 12:52:07 -0400

    Agregar script install.bat para instalar dependencias

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ========================================
+echo   Instalando dependencias
+echo ========================================
+echo.
+echo Este proceso puede tardar unos minutos...
+echo.
+cmd /k npm install
